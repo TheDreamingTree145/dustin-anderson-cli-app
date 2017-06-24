2a1aa9ed9720746682e3c4c47742bdfdf106cbd0
Dustin Anderson <dandersonsoftware@outlook.com>
2017-06-24 16:55:07 +0000

    added way to get link and get noko to scrape individual movie page

--- lib/movie_scraper.rb
+++ lib/movie_scraper.rb
@@ -16,4 +16,7 @@ class MovieScraper
       movie.title = row.css(".unstyled").text
       movie.title.strip!
       movie.reviews = row.css("td.right-hidden-xs").text
+      link = row.css("a").attribute("href").value.sub("/", "")
+      details_doc = BASE_RT_URL + link
+      open_details_doc = Nokogiri::HTML(open(details_doc))
 end
