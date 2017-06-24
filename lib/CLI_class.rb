070984feee2ae249693a06f3ed8a74166aeea23b
Dustin Anderson <dandersonsoftware@outlook.com>
2017-06-24 16:19:02 +0000

    created call method for Controller class and added opening statement

--- lib/CLI_class.rb
+++ lib/CLI_class.rb
@@ -1,7 +1,11 @@
+require_relative './movie_scraper'
+require_relative './dustin_anderson_cli_app'
+
 module DustinAndersonCLIApp
   class Controller
 
     def call
+      puts "Welcome to Rotten Tomatoes top 100 movies! This will list the rank, title, rating, and the number of reviews on Rotten Tomatoes. Once you choose how many movies to see at once, just input the rank of the movie to see the movie consensus. At any time you can type 'exit' to quit the program, or options for a list of options\n\n"
     end
 
   end
