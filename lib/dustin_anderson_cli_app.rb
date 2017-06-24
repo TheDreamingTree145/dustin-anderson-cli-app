d69a1f9889ab4b2de815bb7dfa931c5893b89eb1
Dustin Anderson <dandersonsoftware@outlook.com>
2017-06-24 15:58:14 +0000

    added movie class with initialization

--- lib/dustin_anderson_cli_app.rb
+++ lib/dustin_anderson_cli_app.rb
@@ -1,6 +1,17 @@
 require "./lib/app/version"
 
 module DustinAndersonCLIApp
-        # Your code goes here...
+  class Movie
+    attr_accessor :rank, :title, :rating, :reviews, :consensus
+    @@all = []
+
+    def initialize(rank = nil, title = nil, rating = nil, reviews = nil, consensus = nil)
+      @rank = rank
+      @title = title
+      @rating = rating
+      @reviews = reviews
+      @consensus = consensus
+      @@all << self
+    end
 
 end
