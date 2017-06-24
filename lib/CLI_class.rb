7cc74bbdf663f609dcca6b0a682ac74679475a7f
Dustin Anderson <dandersonsoftware@outlook.com>
2017-06-24 16:26:43 +0000

    added consensus var and rearranged

--- lib/CLI_class.rb
+++ lib/CLI_class.rb
@@ -5,9 +5,16 @@ module DustinAndersonCLIApp
   class Controller
 
     def call
-      puts "Welcome to Rotten Tomatoes top 100 movies! This will list the rank, title, rating, and the number of reviews on Rotten Tomatoes. Once you choose how many movies to see at once, just input the rank of the movie to see the movie consensus. At any time you can type 'exit' to quit the program, or options for a list of options.\n\n"
 
       options = "Here are your options for lists: \n" "To see the top 10, enter: 'top 10'\n" "To see the top 25, enter: 'top 25'\n" "To see the top 50, enter: 'top 50'\n" "To see the full list, enter: 'list'\n\n"
+
+      concensus_reminder = "\nIf you would like to see a movie consensus, enter 'movie consensus' and then the rank of the movie you would like the concensus of.\n\n"
+
+      puts "Welcome to Rotten Tomatoes top 100 movies! This will list the rank, title, rating, and the number of reviews on Rotten Tomatoes. Once you choose how many movies to see at once, just input the rank of the movie to see the movie consensus. At any time you can type 'exit' to quit the program, or options for a list of options.\n\n"
+
+      puts options
+
+
     end
 
 
