301d24ad56d9e3b4981707c7954b0f83fff1b299
Dustin Anderson <dandersonsoftware@outlook.com>
2017-06-24 19:05:45 +0000

    made minor changes to interface

--- lib/dustin_anderson_cli_app.rb
+++ lib/dustin_anderson_cli_app.rb
@@ -19,16 +19,18 @@ module DustinAndersonCLIApp
     end
 
     def self.movie_summary
+      options = "Here are your options for lists: \n" "To see the top 10, enter: 'top 10'\n" "To see the top 25, enter: 'top 25'\n" "To see the top 50, enter: 'top 50'\n" "To see the full list, enter: 'list'\n\n"
       puts "Please enter the rank of the move you would like the critics summary of: "
       summary_input = gets.chomp
       if summary_input.to_i >= 1 && summary_input.to_i <= 100
         self.all.each do |film|
           if film.rank == summary_input
-            puts film.title
+            puts "#{film.title}:"
             puts film.summary
           end
         end
       end
+      puts "\n#{options}"
     end
 
   end
