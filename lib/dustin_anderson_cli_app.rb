08c5ed6393b81dd8492ffcf6370f79841c16a978
Dustin Anderson <dandersonsoftware@outlook.com>
2017-06-24 19:16:44 +0000

    made movie_summary into a loop

--- lib/dustin_anderson_cli_app.rb
+++ lib/dustin_anderson_cli_app.rb
@@ -20,13 +20,18 @@ module DustinAndersonCLIApp
 
     def self.movie_summary
       options = "Here are your options for lists: \n" "To see the top 10, enter: 'top 10'\n" "To see the top 25, enter: 'top 25'\n" "To see the top 50, enter: 'top 50'\n" "To see the full list, enter: 'list'\n\n"
-      puts "Please enter the rank of the move you would like the critics summary of: "
-      summary_input = gets.chomp
-      if summary_input.to_i >= 1 && summary_input.to_i <= 100
-        self.all.each do |film|
-          if film.rank == summary_input
-            puts "#{film.title}:"
-            puts film.summary
+      another_summary = 'yes'
+      while another_summary == 'yes'
+        puts "Please enter the rank of the move you would like the summary of: "
+        summary_input = gets.chomp
+        if summary_input.to_i >= 1 && summary_input.to_i <= 100
+          self.all.each do |film|
+            if film.rank == summary_input
+              puts "#{film.title}:"
+              puts film.summary
+              puts "\nWould you like another movie summary? If so enter 'yes'. Else press 'enter' for list of options."
+              another_summary = gets.chomp
+            end
           end
         end
       end
